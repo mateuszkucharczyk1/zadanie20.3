391789543e5fb258e99540e6d4f3742756619462
Mateusz Kucharczyk <mateusz_kucharczyk@yahoo.com>
2018-09-05 15:38:31 +0200

    fix

--- country-app/src/index.js
+++ country-app/src/index.js
@@ -1,13 +1,5 @@
 import React from 'react';
 import { render } from 'react-dom';
-
-render(
-    <h1>Inicjalizacja projektu</h1>,
-    document.getElementById('root')
-);
-
-import React from 'react';
-import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './store';
 
